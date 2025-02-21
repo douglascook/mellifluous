174659df5d4a11c36994777d7f4c3101b9f8b7d4
Douglas Cook <dugup@hotmail.co.uk>
2025-02-21 16:11:56 +0000

    Add a FastAPI app, with example chart.js plot

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request) -> Any:
+    return templates.TemplateResponse(request=request, name="index.html")
